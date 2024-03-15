a2b9330a399b1921d8a55ac90ea056d6fdd1920d
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-15 17:22:07 +1300

    Add View_CustomerWithIssue Model

---
+++ RepositoryPractice4/Controllers/CustomerListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RepositoryPractice4.Controllers
+{
+    public class CustomerListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RepositoryPractice4/Models/View_CustomerWithIssue.cs
@@ -0,0 +1,14 @@
+﻿namespace RepositoryPractice4.Models
+{
+    public class View_CustomerWithIssue
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public int IssueId { get; set; }
+        public string Issue { get; set; }
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        public string CarRego { get; set; }
+        public string Description { get; set; }
+    }
+}
