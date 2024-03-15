955569c777c29b6a88e6a779e305ad70ab95198d
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-15 17:09:20 +1300

    Add project files.

---
+++ RepositoryPractice4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RepositoryPractice4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
