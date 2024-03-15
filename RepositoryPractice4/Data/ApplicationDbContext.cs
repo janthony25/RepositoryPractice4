cf7e7b637c8d013452de4617740e7530181617dd
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-15 17:17:16 +1300

    Add ApplicationDbContext

---
+++ RepositoryPractice4/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryPractice4.Models;
+
+namespace RepositoryPractice4.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<tblCustomer> tblCustomer { get; set; }
+        public DbSet<tblIssue> tblIssue { get; set; }
+
+    }
+}
