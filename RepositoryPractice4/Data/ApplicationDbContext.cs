3df305c45dff80f130f465031215d964fed57e1f
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-15 17:28:13 +1300

    Add View_CustomerWithIssue to DbContext

--- RepositoryPractice4/Data/ApplicationDbContext.cs
+++ RepositoryPractice4/Data/ApplicationDbContext.cs
@@ -13,5 +13,24 @@ namespace RepositoryPractice4.Data
         public DbSet<tblCustomer> tblCustomer { get; set; }
         public DbSet<tblIssue> tblIssue { get; set; }
 
+        public DbSet<View_CustomerWithIssue> View_CustomerWithIssue { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<View_CustomerWithIssue>(Entity =>
+            {
+                Entity.HasNoKey().ToView("View_CustomerWithIssue");
+                Entity.Property(e => e.CustomerId).HasColumnName("CustomerId");
+                Entity.Property(e => e.CustomerName).HasMaxLength(50).HasColumnName("CustomerName");
+                Entity.Property(e => e.IssueId).HasColumnName("IssueId");
+                Entity.Property(e => e.Issue).HasMaxLength(50).HasColumnName("Issue");
+                Entity.Property(e => e.CarMake).HasMaxLength(15).HasColumnName("CarMake");
+                Entity.Property(e => e.CarModel).HasMaxLength(20).HasColumnName("CarModel");
+                Entity.Property(e => e.CarRego).HasMaxLength(15).HasColumnName("CarRego");
+                Entity.Property(e => e.Description).HasMaxLength(200).HasColumnName("Description");
+                
+            });
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
